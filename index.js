581c5ce602d051e11d819aa034a879b102a12d34
Oussama-Ghomrawi <ouzz1.96@gmail.com>
2021-12-31 19:21:24 +0200

    step-3

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var today = new Date();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
 app.get('/', (req, res) => {
   res.send('ok')
+})
+app.get('/test',(req,res)=>{
+    res.json({"status":200,"message":"ok"})
+})
+app.get('/time',(req,res)=>{
+    res.json({"status":200,"message":time})
 })
 
 app.listen(port, () => {
