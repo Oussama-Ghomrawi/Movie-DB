8559d544cdce0069d2637fed071763803c65e8eb
Oussama-Ghomrawi <ouzz1.96@gmail.com>
2022-01-11 10:02:38 +0200

    step13-1

--- index.js
+++ index.js
@@ -19,6 +19,104 @@ var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds(
 //    { title: 'الإرهاب والكباب', year: 1992, rating: 6.2 }
 //]
 
+//users array
+const users = [
+  { username: 'ouss', password: 'hummus' },
+  { username: 'badih', password: 'shawerma' },
+    ]
+
+//search users
+app.get('users/search', (req, res) => {
+    const search = req.query.s;
+    if (typeof search != 'undefined') {
+        const response = {
+            status: 200, message: "ok", data: search
+        };
+        res.send(response);
+    }
+    else {
+        const response = {
+            status: 500, error: true, message: "you have to provide a search"
+        };
+        res.status(500);
+        res.send(response);
+    }
+});
+
+//users read
+app.get('/users/read', (req, res) => {
+    res.json({ "status": 200, "message": users })
+})
+
+//users sort by username
+app.get('/users/read/by-username', (req, res) => {
+   res.json({
+       "status": 200, "data": users.sort((a, b) => {
+           if (a.username.toLowerCase() < b.username.toLowerCase())
+               return -1
+           if ((a.username.toLowerCase() > b.username.toLowerCase()))
+               return 1
+           return 0
+       })
+   })
+})
+
+//users read by ID
+app.get('/users/read/id/:id', (req, res) => {
+    let id = req.params.id
+    if (id) {
+        if (id < users.length) {
+            res.json({status:200,data:users[id]})
+        }
+        else if(id == null || id=== undefined) {
+            res.json({status:404,error:"You didnt add an id!"})
+        }
+        else{
+            res.status(404).json({status:404,error:true, message:"the uder with " +id+ " does not exist!"} )
+        }
+    }
+})
+
+
+//users create
+app.post('/users/create', (req, res) => {
+    if (!req.query.username || !req.query.password){
+        res.json({ status: 403, error: true, message: 'You cant create an account without a username or without a password' })
+    }    
+    else {
+        let username = req.query.username;
+        let password = req.query.password;
+        users.push({ username, password});
+        res.json({ status: 200, data: users });
+    }
+})
+
+//users delete by ID
+app.delete('/users/delete/:ID', (req, res)=> {
+let deleteduser=req.params.ID
+if (users.length<deleteduser){
+res.json({status:404, error:true, message:'the user with the ' +ID+ ' does not exist'})
+}
+else{
+users.splice(deleteduser, 1)
+res.json ({status:200, data:users})}
+})
+
+
+//users update by ID
+app.put('/users/update/:ID', (req, res) => {
+    var usernamex = req.query.username;
+    var passwordx = req.query.password;
+if(!req.query.username){
+    var usernamex=users[req.params.ID].username
+}
+if(!req.query.password){
+    var passwordx=users[req.params.ID].password
+}
+    users[req.params.ID] = { username: usernamex, password: passwordx };
+    res.json({ status: 200, data: users })
+})
+
 
 const movieshcema = new mongoose.Schema({
     title: { type: String, default: "" },
@@ -26,7 +124,7 @@ const movieshcema = new mongoose.Schema({
     rating: { type: Number, default: 4 },
 }, { versionKey: false });
 const movies = mongoose.model("movies", movieshcema);
-movies.create({ title: 'Jaws', year: 1975, rating: 8 }, { title: 'Avatar', year: 2009, rating: 7.8 }, { title: 'Brazil', year: 1985, rating: 8 }, { title: 'الإرهاب والكباب', year: 1992, rating: 6.2 })
+//movies.create({ title: 'Jaws', year: 1975, rating: 8 }, { title: 'Avatar', year: 2009, rating: 7.8 }, { title: 'Brazil', year: 1985, rating: 8 }, { title: 'الإرهاب والكباب', year: 1992, rating: 6.2 })
 
 
 
